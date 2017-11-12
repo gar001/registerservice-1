6ad5fa68db772ce4a761bf4e6aebc46e07d2ae5b
gar001 <31540933+gar001@users.noreply.github.com>
2017-11-12 16:39:46 -0600

    Add files via upload

---
+++ src/main/java/edu/uark/models/entities/TransactionEntity.java
@@ -0,0 +1,136 @@
+package edu.uark.models.entities;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+
+import edu.uark.dataaccess.entities.BaseEntity;
+import edu.uark.models.api.Transaction;
+import edu.uark.models.entities.fieldnames.TransactionFieldNames; //
+import edu.uark.models.repositories.TransactionRepository; //
+
+public class TransactionEntity extends BaseEntity<TransactionEntity> {
+	@Override
+	protected void fillFromRecord(ResultSet rs) throws SQLException {
+		this.lookupCode = rs.getString(TransactionFieldNames.CASHIER_ID);
+		this.count = rs.getInt(TransactionFieldNames.REFERENCE_ID);
+		this.count = rs.getInt(TransactionFieldNames.TOTAL_AMOUNT);
+		this.count = rs.getInt(TransactionFieldNames.TYPE);
+		this.createdOn = rs.getTimestamp(TransactionFieldNames.CREATED_ON).toLocalDateTime();
+	}
+
+	@Override
+	protected Map<String, Object> fillRecord(Map<String, Object> record) {
+		record.put(TransactionFieldNames.CASHIER_ID, this.cashierId);
+		record.put(TransactionFieldNames.REFERENCE_ID, this.referenceId);
+		record.put(TransactionFieldNames.TOTAL_AMOUNT, this.totalAmount);
+		record.put(TransactionFieldNames.Type, this.type);
+		record.put(TransactionFieldNames.CREATED_ON, Timestamp.valueOf(this.createdOn));		
+
+		return record;
+	}
+	
+	private UUID cashierId;
+	public UUID getCashierId() {
+		return this.id;
+	}
+	public TransactionEntity setCashierId(UUID id) {
+		this.cashierId = id;
+		return this;
+	}
+
+	private UUID referenceId;
+	public UUID getReferenceId() {
+		return this.id;
+	}
+	public TransactionEntity setReferenceId(UUID id) {
+		this.referenceId = id;
+		return this;
+	}
+	
+	private int totalAmount;
+	public int getTotalAmount() {
+		return this.totalAmount;
+	}
+	public TransactionEntity setTotalAmount(int amount) {
+		this.totalAmount = amount;
+		return this;
+	}
+	
+	private String type;
+	public String getType() {
+		return this.type;
+	}
+	public TransactionEntity setType(String type) {
+		this.type = type;
+		return this;
+	}
+
+	private LocalDateTime createdOn;
+	public LocalDateTime getCreatedOn() {
+		return this.createdOn;
+	}
+	public TransactionEntity setCreatedOn(LocalDateTime createdOn) {
+		this.createdOn = createdOn;
+		return this;
+	}
+	
+	private TransactionEntityApiRequestStatus apiRequestStatus;
+	public TransactionEntityApiRequestStatus getApiRequestStatus() {
+		return this.apiRequestStatus;
+	}
+	public TransactionEntity setApiRequestStatus(TransactionEntityApiRequestStatus apiRequestStatus) {
+		if (this.apiRequestStatus != apiRequestStatus) {
+			this.apiRequestStatus = apiRequestStatus;
+		}
+		
+		return this;
+	}
+	
+	private String apiRequestMessage;
+	public String getApiRequestMessage() {
+		return this.apiRequestMessage;
+	}
+	public TransactionEntity setApiRequestMessage(String apiRequestMessage) {
+		if (!StringUtils.equalsIgnoreCase(this.apiRequestMessage, apiRequestMessage)) {
+			this.apiRequestMessage = apiRequestMessage;
+		}
+		
+		return this;
+	}
+	
+	public TransactionEntity() {
+		super(new TransactionRepository());
+		
+		this.cashierId = new UUID(0, 0);
+		this.totalAmount = -1;
+		this.type = StringUtils.EMPTY;
+		this.referenceId = new UUID(0, 0);
+		this.createdOn = LocalDateTime.now();
+	}
+	
+	public TransactionEntity(UUID id) {
+		super(id, new TransactionRepository());
+		
+		this.cashierId = new UUID(0, 0);
+		this.totalAmount = -1;
+		this.type = StringUtils.EMPTY;
+		this.referenceId = new UUID(0, 0);
+		this.createdOn = LocalDateTime.now();
+	}
+
+	public TransactionEntity(Transaction apiTransaction) {
+		super(apiTransaction.getId(), new TransactionRepository());
+
+		this.cashierId = apiTransaction.getCashierId();
+		this.totalAmount = apiTransaction.getTotalAmount();
+		this.type = apiTransaction.getType();
+		this.referenceId = apiTransaction.getReferenceId();
+		this.createdOn = LocalDateTime.now();
+	}
+}
