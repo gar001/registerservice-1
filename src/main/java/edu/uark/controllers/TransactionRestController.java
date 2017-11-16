cc0c6e472e48d99cbd8c82ec57683c28f14cdb23
gar001 <31540933+gar001@users.noreply.github.com>
2017-11-16 11:16:09 -0600

    Add files via upload

---
+++ src/main/java/edu/uark/controllers/TransactionRestController.java
@@ -0,0 +1,39 @@
+
+package edu.uark.controllers;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.uark.commands.transaction.TransactionQuery;
+import edu.uark.commands.transaction.TransactionSaveCommand;
+import edu.uark.models.api.Transaction;
+
+@RestController
+@RequestMapping(value = "/employee")
+public class EmployeeRestController {
+	@RequestMapping(value = "/apiv0/{transactionId}", method = RequestMethod.GET)
+	public Employee getTransaction(@PathVariable UUID transactionId) {
+		return (new transactionQuery()).
+			setTransactionId(transactionId).
+			execute();
+	}
+
+	@RequestMapping(value = "/apiv0/transactions", method = RequestMethod.PUT)
+	public Transaction putTransaction(@RequestBody Transaction transaction) {
+		return (new TransactionSaveCommand()).
+			setApiTransaction(transaction).
+			execute();
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		return "Successful test. (EmployeeRestController)";
+	}
+}
