01a3e4edd239e39502d18db229567e9a1ea409ec
gar001 <31540933+gar001@users.noreply.github.com>
2017-11-16 11:21:44 -0600

    Update TransactionEntity.java

--- src/main/java/edu/uark/models/entities/TransactionEntity.java
+++ src/main/java/edu/uark/models/entities/TransactionEntity.java
@@ -17,15 +17,17 @@ import edu.uark.models.repositories.TransactionRepository; //
 public class TransactionEntity extends BaseEntity<TransactionEntity> {
 	@Override
 	protected void fillFromRecord(ResultSet rs) throws SQLException {
-		this.lookupCode = rs.getString(TransactionFieldNames.CASHIER_ID);
-		this.count = rs.getInt(TransactionFieldNames.REFERENCE_ID);
-		this.count = rs.getInt(TransactionFieldNames.TOTAL_AMOUNT);
-		this.count = rs.getInt(TransactionFieldNames.TYPE);
+		this.recordId = rs.getInt(TransactionFieldNames.RECORD_ID);
+		this.cashierId = rs.getInt(TransactionFieldNames.CASHIER_ID);
+		this.referenceId = rs.getInt(TransactionFieldNames.REFERENCE_ID);
+		this.totalAmount = rs.getInt(TransactionFieldNames.TOTAL_AMOUNT);
+		this.type = rs.getString(TransactionFieldNames.TYPE);
 		this.createdOn = rs.getTimestamp(TransactionFieldNames.CREATED_ON).toLocalDateTime();
 	}
 
 	@Override
 	protected Map<String, Object> fillRecord(Map<String, Object> record) {
+		record.put(TransactionFieldNames.RECORD_ID, this.recordId);
 		record.put(TransactionFieldNames.CASHIER_ID, this.cashierId);
 		record.put(TransactionFieldNames.REFERENCE_ID, this.referenceId);
 		record.put(TransactionFieldNames.TOTAL_AMOUNT, this.totalAmount);
@@ -33,6 +35,15 @@ public class TransactionEntity extends BaseEntity<TransactionEntity> {
 		record.put(TransactionFieldNames.CREATED_ON, Timestamp.valueOf(this.createdOn));		
 
 		return record;
+	}
+
+	private UUID recordId;
+	public UUID getRecordId() {
+		return this.id;
+	}
+	public TransactionEntity setRecordId(UUID id) {
+		this.recordId = id;
+		return this;
 	}
 	
 	private UUID cashierId;
@@ -107,6 +118,7 @@ public class TransactionEntity extends BaseEntity<TransactionEntity> {
 	public TransactionEntity() {
 		super(new TransactionRepository());
 		
+		this.recordId = new UUID(0,0);
 		this.cashierId = new UUID(0, 0);
 		this.totalAmount = -1;
 		this.type = StringUtils.EMPTY;
@@ -117,6 +129,7 @@ public class TransactionEntity extends BaseEntity<TransactionEntity> {
 	public TransactionEntity(UUID id) {
 		super(id, new TransactionRepository());
 		
+		this.recordId = new UUID(0,0);
 		this.cashierId = new UUID(0, 0);
 		this.totalAmount = -1;
 		this.type = StringUtils.EMPTY;
@@ -127,6 +140,7 @@ public class TransactionEntity extends BaseEntity<TransactionEntity> {
 	public TransactionEntity(Transaction apiTransaction) {
 		super(apiTransaction.getId(), new TransactionRepository());
 
+		this.recordId = apiTransaction.getRecordId();
 		this.cashierId = apiTransaction.getCashierId();
 		this.totalAmount = apiTransaction.getTotalAmount();
 		this.type = apiTransaction.getType();
