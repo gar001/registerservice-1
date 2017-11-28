12aece4561a2490b037f7dcaa7ff17b6a5405023
gar001 <31540933+gar001@users.noreply.github.com>
2017-11-28 12:30:58 -0600

    Add files via upload

---
+++ src/main/java/edu/uark/commands/transactionEntries/CreateTransactionCommand.java
@@ -0,0 +1,33 @@
+package edu.uark.commands.transactionEntry;
+
+import java.util.UUID;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import edu.uark.commands.ResultCommandInterface;
+import edu.uark.models.api.Transaction;
+import edu.uark.models.api.TransactionEntry;
+import edu.uark.models.api.enums.TransactionEntryApiRequestStatus; //
+import edu.uark.models.entities.TransactionEntryEntity;
+import edu.uark.models.repositories.TransactionEntryRepository;
+import edu.uark.models.repositories.interfaces.TransactionEntryRepositoryInterface;	
+
+public class CreateTransactionCommand {
+
+	public Transaction execute() {
+		return this.apiTransactionEntry.createTransaction();
+	}
+
+	private TransactionEntry apiTransactionEntry
+	public TransactionEntry getApiTransactionEntry() {
+		return this.apiTransactionEntry;
+	}
+	public TransactionSaveCommand setApiTransactionEntry(TransactionEntry apiTransactionEntry) {
+		this.apiTransactionEntry = apiTransactionEntry;
+		return this;
+	}
+
+	public TransactionEntrySaveCommand() {
+	}
+}
---
+++ src/main/java/edu/uark/commands/transactionEntries/TransactionEntrySaveCommand.java
@@ -0,0 +1,75 @@
+package edu.uark.commands.transactionEntry;
+
+import java.util.UUID;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import edu.uark.commands.ResultCommandInterface;
+import edu.uark.models.api.Transaction;
+import edu.uark.models.api.TransactionEntry;
+import edu.uark.models.api.enums.TransactionEntryApiRequestStatus; //
+import edu.uark.models.entities.TransactionEntryEntity;
+import edu.uark.models.repositories.TransactionEntryRepository;
+import edu.uark.models.repositories.interfaces.TransactionEntryRepositoryInterface;
+
+public class TransactionSaveCommand implements ResultCommandInterface<TransactionEntry> {
+	@Override
+	public TransactionEntry execute() {
+		
+		TransactionEntity transactionEntity;
+		if (StringUtils.isBlank(this.apiTransactionEntry.getTransactionEntryId())) {
+			String newId;
+			do {
+				newId = RandomStringUtils.randomNumeric(ID_LENGTH);
+			} while (this.transactionEntryRepository.transactionEntryExists(newId));
+
+			this.apiTransactionEntry.setId(newId);
+			transactionEntryEntity = new TransactionEntryEntity(this.apiTransactionEntry);
+		} else {
+			transactionEntryEntity = this.transactionEntryRepository.get(this.apiTransactionEntry.getId());
+			if (transactionEntity != null) {
+				this.apiTransactionEntry = transactionEntryEntity.synchronize(this.apiTransactionEntry);
+			} else {
+				transactionEntryEntity = this.transactionEntryRepository.byId/**/(this.apiTransactionEntry.getId());
+				if (transactionEntryEntity == null) {
+					transactionEntryEntity = new transactionEntryEntity(this.apiTransactionEntry);
+				} else {
+					return (new TransactionEntry()).setApiRequestStatus(TransactionEntryApiRequestStatus.ID_ALREADY_EXISTS);
+				}
+			}
+		}
+		
+		transactionEntity.save();
+		if ((new UUID(0, 0)).equals(this.apiTransactionEntry.getId())) {
+			this.apiTransactionEntry.setId(transactionEntryEntity.getId());
+		}
+		
+		return this.apiTransactionEntry;
+	}
+	
+	//Properties
+	private TransactionEntry apiTransactionEntry;
+	public TransactionEntry getApiTransactionEntry() {
+		return this.apiTransactionEntry;
+	}
+	public TransactionSaveCommand setApiTransactionEntry(TransactionEntry apiTransactionEntry) {
+		this.apiTransactionEntry = apiTransactionEntry;
+		return this;
+	}
+	
+	private TransactionEntryRepositoryInterface transactionEntryRepository;
+	public TransactionEntryRepositoryInterface getTransactionEntryRepository() {
+		return this.transactionEntryRepository;
+	}
+	public TransactionEntrySaveCommand setTransactionEntryRepository(TransactionEntryRepositoryInterface transactionEntryRepository) {
+		this.transactionEntryRepository = transactionEntryRepository;
+		return this;
+	}
+	
+	private static final int ID_LENGTH = 4;
+	
+	public TransactionEntrySaveCommand() {
+		this.transactionEntryRepository = new TransactionEntryRepository();
+	}
+}
