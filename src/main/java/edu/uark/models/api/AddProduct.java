37a26e8cd0f28f77278c1d0e007f254c311cff81
gar001 <31540933+gar001@users.noreply.github.com>
2017-11-28 11:57:14 -0600

    Add files via upload

---
+++ src/main/java/edu/uark/models/api/AddProduct.java
@@ -0,0 +1,27 @@
+package edu.uark.models.api;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class RemoveProduct {
+	private String productLookUpCode;
+	public String getProductLookUpCode() {
+		return this.productLookUpCode;
+	}
+	public EmployeeLogin setProductLookUpCode(String productLookUpCode) {
+		this.productId = productId;
+		return this;
+	}
+	private TransactionEntry transactionEntry;
+	public TransactionEntry getTransactionEntry() {
+		return this.transactionEntry;
+	}
+	public EmployeeLogin setTransactionEntry(TransactionEntry transactionEntry) {
+		this.transactionEntry = new TransactionEntry(transactionEntry);
+		return this;
+	}
+	
+	public EmployeeLogin() {
+		this.productLookUpCode = StringUtils.EMPTY;
+		this.transactionEntry = new TransactionEntry();
+	}
+}
